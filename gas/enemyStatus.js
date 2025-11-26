e8f8479a315f0554aba823e2b8f7d6a7fb3da743
guu <Yu0gura1624@outlook.jp>
2025-11-27 02:22:26 +0900

    敵のステータスプロパティの列順を修正し、RarityをHPの前に移動

--- gas/enemyStatus.js
+++ gas/enemyStatus.js
@@ -2,7 +2,14 @@ const enemyStatusInitRowIndex = 2;
 
 const enemyStatusProp = {
     Type: { column: 1 },
-    HP:        { column: 2 },
-    AttackPower: { column: 3 },
-    Speed:     { column: 4 },
+    Rarity: { column: 2 },
+    HP:        { column: 3 },
+    AttackPower: { column: 4 },
+    Speed:     { column: 5 },
+    AblePlayerDistance:     { column: 6 },
+    MinSpawnPositionX:     { column: 7 },
+    MaxSpawnPositionX:     { column: 8 },
+    MinSpawnPositionY:     { column: 9 },
+    MaxSpawnPositionY:     { column: 10 },
+    PoolSize:     { column: 11 },
 };
